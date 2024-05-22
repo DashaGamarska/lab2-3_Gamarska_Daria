7051ce6d1468bd6608e4027ac3d081c5d72b41f1
DashaGamarska <vt211_gdi@student.ztu.edu.ua>
2024-05-22 22:33:26 +0300

    lab2-3

---
+++ lab2-3/my-react-site/src/App.js
@@ -0,0 +1,184 @@
+import Layer12 from ".//photo/Layer12.png";
+import Layer10 from ".//photo/Layer10.png";
+import Layer11 from ".//photo/Layer11.png";
+import Layer13 from ".//photo/Layer13.png";
+import Layer17 from ".//photo/Layer17.png";
+import front from ".//photo/front.png";
+import screen from ".//photo/screen.png";
+import refelection from ".//photo/refelection.png";
+import Layer14 from ".//photo/Layer14.png";
+import Layer15 from ".//photo/Layer15.png";
+import Shape6 from ".//photo/Shape6.png";
+import stars from ".//photo/stars.png";
+import ADVBG from ".//photo/ADVBG.png";
+import TRANSPARANT_BG from "./photo/TRANSPARANT_BG.png";
+import Layer16 from ".//photo/Layer16.png";
+import Shape8 from ".//photo/Shape8.png";
+import Photo_1 from ".//photo/Photo_1.png";
+import Photo_2 from ".//photo/Photo_2.png";
+import Layer2 from ".//photo/Layer2.png";
+import Layer7 from ".//photo/Layer7.png";
+import Layer4 from ".//photo/Layer4.png";
+import Layer6 from ".//photo/Layer6.png";
+import Layer5 from ".//photo/Layer5.png";
+import Layer3 from ".//photo/Layer3.png";
+import Layer8 from ".//photo/Layer8.png";
+import Layer9 from ".//photo/Layer9.png";
+import './App.css';
+
+function App() {
+  return (
+      <div className="MENU_BG">
+          <header>
+              <div className="MENU_BG">
+
+                  <a href="/" className="HOME_">Home</a>
+                  <a href="/about" className="ABOUT_">About</a>
+                  <a href="/blog" className="BLOG_">Blog</a>
+                  <a href="/contact" className="CONTACT_">Contact</a>
+                  <a href="/register" className="REGISTER___LOG_IN">REGISTER LOGIN</a>
+
+              </div>
+          </header>
+          <main>
+              <div>
+                  <img src={Layer14} alt="layer14" className="Layer_14"/>
+                  <img src={Layer15} alt="layer15" className="Layer_15"/>
+                  <img src={Layer17} alt="layer17" className="Layer_17"/>
+                  <img src={front} alt="front" className="front"/>
+                  <img src={screen} alt="screen" className="screen"/>
+                  <img src={front} alt="front" className="front"/>
+                  <img src={refelection} alt="refelection" className="refelection"/>
+                  <img src={TRANSPARANT_BG} alt="TRANSPARANT_BG" className="TRANSPARANT_BG"/>
+                  <img src={Layer16} alt="Layer16" className="Layer_16"/>
+                  <p className="Portfolio_Design">Portfolio Design</p>
+                  <p className="In_Wordpress_Themes__by_gerbengeeraerts__on_20t"> In Wordpress Themes, by
+                      gerbengeeraerts, on 20th Septemer 2012</p>
+                  <p className="Lorem_ipsum_dolor_sit_amet__consectetur_adipisc">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id eros
+                      nibh. Fusce ut augue nunc. Vestibulum ante ipsum primis in faucibus orci
+                      luctus et ultrices posuere cubilia Curae; Praesent viverra laoreet enim
+                      sagittis sodales. Proin pharetra felis sed magna feugiat pellentesque.
+
+                      Aliquam eget nulla vitae felis dictum consequat. Pellentesque laoreet
+                      erat placerat augue dignissim a molestie sapien egestas. Quisque ac
+                      scelerisque velit. Pellentesque ligula orci, vehicula in interdum ut,
+                      molestie in lectus. Cras aliquet augue eget ipsum ullamcorper vulputate.
+                      Curabitur semper volutpat vulputate. Morbi neque leo, suscipit euismod
+                      vestibulum ac, suscipit ut mi. Nullam sapien quam, iaculis vitae egesta
+                      accumsan,
+                  </p>
+
+
+              </div>
+              <div>
+
+                  <img src={Layer12} alt="layer12" className="Layer_12"/>
+                  <img src={Layer10} alt="layer10" className="Layer_10"/>
+                  <img src={Layer11} alt="layer11" className="Layer_11"/>
+                  <img src={Layer13} alt="layer13" className="Layer_13"/>
+                  <p className="Why_choose_WebOnDemand_">Why choose WebOnDemand?</p>
+                  <img src={Shape8} alt="Shape8" className="Shape_8"/>
+                  <img src={Shape9} alt="Shape9" className="Shape_9"/>
+                  <p className="Lorem_ipsum_dolor_sit_amet__consectetur_adipisc2"> Lorem ipsum dolor sit amet,
+                      consectetur adipiscing elit. Nunc facilisis viverra adipiscing. Aenean enim urna, consectetur ut
+                      pharetra id, bibendum nec enim.Morbi
+                      id diam nisi, vel interdum ipsum. Phasellus turpis nunc, mattis ut commodo molestie, tristique sit
+                      amet sem. Mauris nisi lectus, consequat elementum commodo
+                      eget, fringilla non ipsum. Ut in lectus eu mi ultricies sagittis at eget nibh. Nam vitae risus
+                      leo, sit amet euismod lorem. Maecenas vitae ligula vitae felis sagittis
+                      semper. Sed sit amet libero a diam tempus gravida. Cras venenatis libero at dolor cursus vitae
+                      ullamcorper nulla dictum. Donec euismod dolor sed lectus
+                      vestibulum porttitor. Nunc mi augue, faucibus ut sagittis vitae, vestibulum eget sem. Lorem ipsum
+                      dolor sit amet, consectetur adipiscing elit. Donec at nisl nulla,
+                      non pharetra libero. Nam enim massa, accumsan nec feugiat at, accumsan vitae augue. Fusce diam
+                      urna, aliquet vel dictum ac, lobortis sit amet est.
+                  </p>
+
+                  <p className="More_projects">More projects</p>
+                  <p className="Testimonials">Testimonials</p>
+                  <p className="Request_a_design">Request a design</p>
+                  <p className="Newsletter"> Newsletter</p>
+                  <p className="Integer_quis_rutrum_tortor__Curabitur_lobortis_">Integer quis rutrum tortor. Curabitur
+                      lobortis erat ac enim
+                      porttitor tempus. Morbi luctus tempus porta. Vestibulum
+                      ante ipsum primis in faucibus orci luctus et ultrices
+                      posuerecubilia Curae; Vivamus elementum lacinia
+                      sodales.Aenean sagittis, ante sed ullamcorper feugiat,
+                      risus nibh venenatis ante, acvarius quam erat vitae odio.</p>
+                  <p className="Integer_eu_ante_in_dolor_vestibulum_auctor__Nul">Integer eu ante in dolor vestibulum
+                      auctor. Nulla
+                      elementum mauris at eros gravida ac accumsan
+                      tellus semper. Cras tortor quam, ullamcorper nec facilisis
+                      eget, ultricies sed sem. Donec non bibendum arcu.
+                      Vestibulum viverra tincidunt enim, at lobortis urna
+                      malesuada et. Fusce sapien diam, ultrices et vehicula at,
+                      gravida vitae odio. Suspendisse potenti. Cum sociis
+                      penatibus et magnis dis parturient montes, nascetur
+                      ridiculus mus. Quisque sit amet commodo nibh.</p>
+                  <p className="Always_wondered_how_something_you_saw_on_the_we">Always wondered how something you saw
+                      on the web is realised? C</p>
+                  <p className="Be_the_first_to_know_our_latest_updates_">Be the first to know our latest updates!</p>
+                  <p className="by_gerbengeeraerts_at_20th_May_2010"> by gerbengeeraerts at 20th May 2010</p>
+                  <p className="by_gerbengeeraerts_at_20th_May_2010_copy "> by gerbengeeraerts at 20th May 2010 copy</p>
+                  <img src={Shape6} alt="shape6" className="Shape_6"/>
+                  <img src={Photo_1} alt="photo_1" className="Photo_1"/>
+                  <img src={Photo_2} alt="photo_2" className="Photo_2"/>
+                  <input className="INPUTBG" placeholder="emailadress"/>
+                  <a className="" href="https://ztu.edu.ua/">
+                      <button className="Contact_us_">Contact us!</button>
+                  </a>
+                  <a className="" href="https://learn.ztu.edu.ua/">
+                      <button className="Subscribe_">Subscribe!</button>
+                  </a>
+
+              </div>
+              <div>
+                  <img src={Shape6} alt="shape6" className="Shape_6"/>
+                  <img src={stars} alt="stars" className="stars"/>
+                  <img src={ADVBG} alt="ADVBG" className="ADVBG"/>
+                  <p className="advertisement">advertisement</p>
+                  <p className="WebOnDemand">WebOnDemand</p>
+                  <p className="Your_ideas_live_in_no_time_">Your ideas live in no-time!</p>
+
+              </div>
+
+          </main>
+          <footer className="FooterBG">
+
+
+          </footer>
+          <p className="Follow_us">Follow us:</p>
+          <a className="" href="https://ztu.edu.ua/">
+              <img src={Layer2} alt="layer2" className="Layer_2"/>
+          </a>
+          <a className="" href="https://ztu.edu.ua/">
+              <img src={Layer7} alt="layer7" className="Layer_7"/>
+          </a>
+          <a className="" href="https://ztu.edu.ua/">
+              <img src={Layer4} alt="layer4" className="Layer_4"/>
+          </a>
+          <a className="" href="https://ztu.edu.ua/">
+          <img src={Layer5} alt="layer5" className="Layer_5"/>
+          </a>
+          <a className="" href="https://ztu.edu.ua/">
+          <img src={Layer8} alt="layer8" className="Layer_8"/>
+          </a>
+          <a className="" href="https://www.facebook.com/www.zhitomir.info/?locale=uk_UA">
+          <img src={Layer6} alt="layer6" className="Layer_6"/>
+          </a>
+          <a className="" href="https://ztu.edu.ua/">
+          <img src={Layer3} alt="layer3" className="Layer_3"/>
+          </a>
+          <p className="Copyright_2010_WebOnDemand___gerbengeeraerts_be">Copyright 2010 WebOnDemand -
+              gerbengeeraerts.be All rights rese</p>
+          <div>
+
+          </div>
+
+      </div>
+
+  );
+}
+
+export default App;
